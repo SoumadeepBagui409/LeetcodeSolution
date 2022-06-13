d6e4fe1ef06ebc9542dce3c0743f30ad6e2d86b7
SoumadeepBagui409 <75625374+SoumadeepBagui409@users.noreply.github.com>
2022-06-13 10:41:10 +0530

    Time: 41 ms (25.53%), Space: 14.9 MB (23.44%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,34 +1,12 @@
 class Solution {
 public:
-    bool cycle(map<int,vector<int>> &adj,int src,vector<bool> &visited, map<int,bool> &pathChecker){
-        visited[src] = true;
-        pathChecker[src] = true;
-        bool ans = false;
-        for(auto x:adj[src]){
-            if(!visited[x]){
-                ans = ans | cycle(adj,x,visited,pathChecker); 
-            }else if(pathChecker[x]){
-                    return true;
-            }
-        }
-        return ans;
-    }
+
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         map<int,vector<int>> adj;
         for(auto x:prerequisites){
             adj[x[1]].push_back(x[0]);
         }
-        map<int,bool> pathCheck;
-        vector<bool> visited(numCourses,-1);
-        bool aCyclic = true;
-        for(int i = 0;i<numCourses;i++){
-            if(!visited[i])
-            aCyclic = aCyclic & !cycle(adj,i,visited,pathCheck);
-        }
-        if(!aCyclic){
-            vector<int> garbage;
-            return garbage;
-        }
+
         vector<int> inOrder(numCourses,0);
         for(int i = 0 ; i < numCourses; i++){
             for(auto x:adj[i]){
