03f4b7b42f86f91a5dac49b647b7ac846a31cd86
SoumadeepBagui409 <75625374+SoumadeepBagui409@users.noreply.github.com>
2022-07-22 10:42:28 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (72.91%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    int cal(string s,int idx,vector<int> &cache){
-        if(s[idx]=='0')return 0;
-        if(idx==s.size())return 1;
-        if(idx>s.size())return 0;
-        if(cache[idx]!=-1)return cache[idx];
-        int op1,op2;
-        op1 = op2 = 0;
-        op1 = cal(s,idx+1,cache);
-        if(idx+1<s.size() and (s[idx]-'0')* 10+(s[idx+1]-'0')<=26)
-        op2 = cal(s,idx+2,cache);
-        return cache[idx] = op1 + op2;
-    }
-    int numDecodings(string s) {
-        vector<int> cache(s.size()+1,-1);
-        return cal(s,0,cache);
+        int numDecodings(string s) {
+        vector<int> cache(s.size(),0);
+            if(s[0]!='0')
+            cache[0] = 1;
+            if(s.size()>1){
+                if(s[1]=='0'){
+                    if( (s[0]-'0')*10+(s[1]-'0')<=26 and s[0]!='0'){
+                    cache[1]++;
+                }
+                }
+                else{
+                    cache[1]+=cache[0];
+                    if(s[0]!='0' and (s[0]-'0')*10+(s[1]-'0')<=26)cache[1]++;
+                }
+            }
+        for(int i = 2; i<s.size();i++){
+            if(s[i] == '0'){
+                if(i-2>=0 and (s[i-1]-'0')*10+(s[i]-'0')<=26 and s[i-1]!='0'){
+                    cache[i] += cache[i-2];
+                }
+            }else{
+                cache[i]+=cache[i-1];
+                if(i-2>=0 and (s[i-1]-'0')*10+(s[i]-'0')<=26 and s[i-1]!='0'){
+                    cache[i]+=cache[i-2];
+                }
+            }
+        }
+        return cache[s.size()-1];
     }
 };
